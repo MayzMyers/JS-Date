3ff633a093090b8181445f9fd2c8f813f7b08ba4
MayzMyers <35607480+MayzMyers@users.noreply.github.com>
2023-11-06 16:52:57 +0300

    Изменение файла JS

---
+++ JS/date.js
@@ -0,0 +1,31 @@
+function task1() {
+    alert(`task1`);
+  }
+  
+  function task2() {
+    alert(`task2`);
+  }
+  
+  function task3() {
+    alert(`task3`);
+  }
+  
+  function task4() {
+    alert(`task4`);
+  }
+  
+  function task5() {
+    alert(`task5`);
+  }
+  
+  function task6() {
+    alert(`task6`);
+  }
+  
+  function task7() {
+    alert(`task7`);
+  }
+  
+  function task8() {
+    alert(`task8`);
+  }
